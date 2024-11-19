d1e7e4dd5f46dcfcff783e003893b67bfe7fe9ed
Avinash <avinashgbadiger@gamil.com>
2024-11-19 22:04:48 +0530

    This is Add Employee  button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
  
 import './App.css'
  
 import ListEmployeeComponent from './assets/components/ListEmployeeComponent'
+import HeaderComponent from './assets/components/HeaderComponent'
+import FooterComponents from './assets/components/FooterComponents'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import EmployeeComponent from './assets/components/EmployeeComponent'
 
 function App() {
    
   return (
     <>
+     <BrowserRouter>
+      
+    <HeaderComponent />
+       <Routes>
+        {/* // http://localhost:5000/ */}
+
+ {/* // http://localhost:5000/emp */}
+
+
+{/* //http://localhost:5000/add-employee */}
+<Route path='/add-employee' element={ <EmployeeComponent /> }></Route>
+
+</Routes>
+       
         <ListEmployeeComponent/>
+        <FooterComponents />
+        </BrowserRouter>
     </>
   )
 }
